2e85216898a6bfa1b6517d515cc95c6273e09f1a
Amy Martin <amymartinm97@ucla.edu>
2023-12-05 16:21:58 -0800

    Added Alfredos Ironwork to list of projects

--- src/components/projects/AlfredosIronwork.js
+++ src/components/projects/AlfredosIronwork.js
@@ -2,4 +2,16 @@ import React from "react";
 
 
 export const AlfredosIronwork = () => {
+    const projectInfo = {
+        title: "Maru's Crochet",
+        id: 'marus-crochet',
+        description: "Alfredo's Ironwork is an interactive web app for a small business where potential clients can learn more about the business' services as well as submit estimate inquiries that are sent directly to the employees in charge. I collaborated with the company to conceptualize, design, and develop both the website and the overall branding.",
+        imgList: [require("../../media/alfredos-ironwork-1"), require("../../media/alfredos-ironwork-2"), require("../../media/alfredos-ironwork-3"), require("../../media/alfredos-ironwork-4")],
+        linkName: 'Github Repository',
+        link:"https://github.com/amy-martin/marus-crochet/",
+    }
+
+    return (
+        <ProjectCard projectInfo={projectInfo} />
+    )
 }
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { MarusCrochet } from "./MarusCrochet";
 import { RedditClient } from "./RedditClient";
+import {AlfredosIronwork} from "./AlfredosIronwork"
 
 export const Projects = () => {
     return (
         <div className="projects">
             <h2 className="page-title">Projects</h2>
+            <AlfredosIronwork />
             <MarusCrochet />
             <RedditClient />
         </div>
