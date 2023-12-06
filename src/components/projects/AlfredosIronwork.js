ca1d9251cbddf05af3030bcbf9595eda76944cc3
Amy Martin <amymartinm97@ucla.edu>
2023-12-05 16:30:17 -0800

    Fixed Alfredos Ironwork details

--- src/components/projects/AlfredosIronwork.js
+++ src/components/projects/AlfredosIronwork.js
@@ -1,14 +1,15 @@
 import React from "react";
+import { ProjectCard } from "./ProjectCard";
 
 
 export const AlfredosIronwork = () => {
     const projectInfo = {
-        title: "Maru's Crochet",
-        id: 'marus-crochet',
+        title: "Alfredo's Ironwork",
+        id: 'alfredos-ironworok',
         description: "Alfredo's Ironwork is an interactive web app for a small business where potential clients can learn more about the business' services as well as submit estimate inquiries that are sent directly to the employees in charge. I collaborated with the company to conceptualize, design, and develop both the website and the overall branding.",
-        imgList: [require("../../media/alfredos-ironwork-1"), require("../../media/alfredos-ironwork-2"), require("../../media/alfredos-ironwork-3"), require("../../media/alfredos-ironwork-4")],
-        linkName: 'Github Repository',
-        link:"https://github.com/amy-martin/marus-crochet/",
+        imgList: [require("../../media/alfredos-ironwork-1.png"), require("../../media/alfredos-ironwork-2.png"), require("../../media/alfredos-ironwork-3.png"), require("../../media/alfredos-ironwork-4.png")],
+        linkName: "Alfredo's Ironwork",
+        link: 'https://alfredosironwork.com/',
     }
 
     return (
