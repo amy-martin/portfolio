e11b340b3a39fb64e8ffe5de4da74e9ca60b226c
Amy Martin <amymartinm97@ucla.edu>
2023-12-06 16:09:56 -0800

    Added projects pagee link to homepage excerpt

--- src/components/homePage/HomePage.js
+++ src/components/homePage/HomePage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Skills } from "./Skills";
+import { Link } from "react-router-dom";
 
 
 export const HomePage = () => {
@@ -9,7 +10,7 @@ export const HomePage = () => {
                     Welcome
                 </h1>
                 <p>
-                    My name is Amy Martin. I am a Los Angeles-based full-stack developer with a passion for crafting dynamic and responsive web applications. Specializing in PostgreSQL, Express, React, Node.js, I thrive on bringing ideas to life through code. Explore my projects to witness the seamless fusion of creativity and functionality in the digital realm.
+                    My name is Amy Martin. I am a Los Angeles-based full-stack developer with a passion for crafting dynamic and responsive web applications. Specializing in PostgreSQL, Express, React, Node.js, I thrive on bringing ideas to life through code. Explore my <Link to='/portfolio/projects'>projects</Link> to witness the seamless fusion of creativity and functionality in the digital realm.
                 </p>
                 <Skills />
         </div>
