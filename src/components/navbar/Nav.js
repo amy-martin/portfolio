f26b12060f2fda7638d1607813db9e812c220b75
Amy Martin <amymartinm97@ucla.edu>
2023-12-27 15:45:29 -0500

    Added logo to act as home link

--- src/components/navbar/Nav.js
+++ src/components/navbar/Nav.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { ContactNav } from "./ContactNav";
 import { SidebarIcon } from "./SidebarIcon";
+import { Link } from "react-router-dom";
 
 export const Nav = () => {
     return (
         <nav>
           <ContactNav />
+          <Link className='logo' to='/portfolio'><img src={require('../../media/logo.png')}/></Link>
           <div className="sidebar-icon-container">
             <SidebarIcon />
           </div>
